c8c0b943cfebfdc9dfd8da0c39f71547ced17306
Tarık Esen <41027583+bytarikesen81@users.noreply.github.com>
2022-06-24 20:29:15 +0300

    Create InvalidInputException.java

---
+++ src/exceptions/InvalidInputException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+public class InvalidInputException extends RuntimeException{
+  public InvalidException(String msg){
+   super(msg); 
+  }
+}
